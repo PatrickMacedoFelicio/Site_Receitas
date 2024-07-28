c4a6b281b01aaec0337ff138d0a75436451db97e
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2024-07-28 15:13:09 -0400

    up carrosel

---
+++ Javascript/slide.js
@@ -0,0 +1,46 @@
+const carouselImages = document.querySelector('.carousel-images');
+const totalImages = document.querySelectorAll('.carousel-images img').length;
+const indicators = document.querySelectorAll('.indicator');
+
+let index = 0;
+
+function moveToNextImage() {
+    index = (index + 1) % totalImages;
+    updateCarousel();
+}
+
+function moveToPreviousImage() {
+    index = (index - 1 + totalImages) % totalImages;
+    updateCarousel();
+}
+
+function updateCarousel() {
+    const offset = -index * 100;
+    carouselImages.style.transform = `translateX(${offset}vw)`;
+
+    // Atualiza os indicadores
+    indicators.forEach(indicator => indicator.classList.remove('active'));
+    indicators[index].classList.add('active');
+}
+
+// Inicializa os indicadores
+updateCarousel();
+
+// Troca de imagem a cada 3 segundos (3000 milissegundos)
+let interval = setInterval(moveToNextImage, 3000);
+
+document.querySelector('.carousel').addEventListener('mouseover', () => {
+    clearInterval(interval);
+});
+
+document.querySelector('.carousel').addEventListener('mouseout', () => {
+    interval = setInterval(moveToNextImage, 3000);
+});
+
+// Adiciona a funcionalidade de navegação pelos indicadores
+indicators.forEach(indicator => {
+    indicator.addEventListener('click', () => {
+        index = parseInt(indicator.getAttribute('data-index'));
+        updateCarousel();
+    });
+});
