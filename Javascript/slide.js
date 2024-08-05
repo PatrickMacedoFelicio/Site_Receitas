ebff9693ebc6a2a49645a8b08e66b1c7d5175844
Patrick Macêdo <patrickmacedofeliceo@gmail.com>
2024-08-05 00:05:09 -0400

    p

--- Javascript/slide.js
+++ Javascript/slide.js
@@ -6,15 +6,15 @@ let index = 0;
 
 function moveToNextImage() {
     index = (index + 1) % totalImages;
-    updateCarousel();
+    atualizarCarousel();
 }
 
 function moveToPreviousImage() {
     index = (index - 1 + totalImages) % totalImages;
-    updateCarousel();
+    atualizarCarousel();
 }
 
-function updateCarousel() {
+function atualizarCarousel() {
     const offset = -index * 100;
     carouselImages.style.transform = `translateX(${offset}vw)`;
 
@@ -24,7 +24,7 @@ function updateCarousel() {
 }
 
 // Inicializa os indicadores
-updateCarousel();
+atualizarCarousel();
 
 // Troca de imagem a cada 3 segundos (3000 milissegundos)
 let interval = setInterval(moveToNextImage, 4500);
@@ -41,6 +41,27 @@ document.querySelector('.carousel').addEventListener('mouseout', () => {
 indicators.forEach(indicator => {
     indicator.addEventListener('click', () => {
         index = parseInt(indicator.getAttribute('data-index'));
-        updateCarousel();
+        atualizarCarousel();
     });
 });
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+
+  
